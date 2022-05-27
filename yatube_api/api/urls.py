4dccac17cdcf4a1da5a276af354eaf76e1fbe3bc
Сергей Кастов <smailix1@gmail.com>
2022-05-27 17:16:15 +0300

    11

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,6 +13,5 @@ router.register('groups', GroupViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
