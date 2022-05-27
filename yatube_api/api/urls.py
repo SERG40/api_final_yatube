3fda83214577f6c1addf8f1cb6c55223ff43b7a5
Сергей Кастов <smailix1@gmail.com>
2022-05-27 17:17:50 +0300

    1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,7 +9,7 @@ router.register('posts', PostViewSet, basename='posts')
 router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                 basename='comments')
 router.register('follow', FollowViewSet, basename='follow')
-router.register('groups', GroupViewSet)
+router.register('groups', GroupViewSet, basename='group')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
